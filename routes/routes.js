5286f5e8dae4016f99a379d935734587362dbc78
Pietro Carta <pietro.carta88@gmail.com>
2019-05-16 13:52:27 +0100

    fix hire routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ import loginRoutes from './loginRoutes';
 import userRoutes from './userRoutes';
 import companyRoutes from './companyRoutes';
 import personRoutes from './personRoutes';
+import hireRoutes from './hireRoutes';
 
 export default function (app) {
   app.use('/api/auth', loginRoutes);
   app.use('/api/users', userRoutes);
   app.use('/api/companies', companyRoutes);
   app.use('/api/people', personRoutes);
+  app.use('/api/hired', hireRoutes);
 }
